2661a789578cae8d984b798f7a28e0f8532e9ee5
GuildedCucumber <hmartinezpi@uoc.edu>
2025-12-16 19:56:10 +0100

    Update End Scene

--- 2D-Platformer-Accessibility/Assets/Scripts/PlatformerGame/WorldMechanics/Door.cs
+++ 2D-Platformer-Accessibility/Assets/Scripts/PlatformerGame/WorldMechanics/Door.cs
@@ -7,20 +7,12 @@ namespace PlatformerGame.WorldMechanics
         [Header("Door Settings")]
         [SerializeField] private bool isOpen = true;
         [SerializeField] private Transform destination;
-        
-        [Header("Visuals")]
-        [SerializeField] private Sprite openSprite;
-        [SerializeField] private Sprite closedSprite;
-        [SerializeField] private Collider2D doorCollider;
-        
-        private SpriteRenderer spriteRenderer;
-        private Animator doorAnimator;
+           
+        private AudioSource audio;
         
         private void Awake()
         {
-            spriteRenderer = GetComponent<SpriteRenderer>();
-            doorAnimator = GetComponent<Animator>();
-            UpdateDoorState();
+            audio = GetComponent<AudioSource>();
         }
         
         public bool CanInteract()
@@ -35,56 +27,12 @@ namespace PlatformerGame.WorldMechanics
             // Teleport player
             interactor.transform.position = destination.position;
             
-            // Optional: Play teleport animation/sound
-            if (doorAnimator != null)
+            if (audio != null)
             {
-                doorAnimator.SetTrigger("Teleport");
+                audio.Play();
             }
             
             Debug.Log("Player teleported through door");
-        }
-        
-        public void OpenDoor()
-        {
-            isOpen = true;
-            UpdateDoorState();
-        }
-        
-        public void CloseDoor()
-        {
-            isOpen = false;
-            UpdateDoorState();
-        }
-        
-        private void UpdateDoorState()
-        {
-            if (spriteRenderer != null)
-            {
-                spriteRenderer.sprite = isOpen ? openSprite : closedSprite;
-            }
-            
-            if (doorCollider != null)
-            {
-                doorCollider.enabled = !isOpen;
-            }
-            
-            if (doorAnimator != null)
-            {
-                doorAnimator.SetBool("IsOpen", isOpen);
-            }
-        }
-        
-        // Call this from levers or other triggers
-        public void ToggleDoor()
-        {
-            if (isOpen)
-            {
-                CloseDoor();
-            }
-            else
-            {
-                OpenDoor();
-            }
         }
     }
 }
